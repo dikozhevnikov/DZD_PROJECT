14104848e9216be94ea0d2e5e4096895ab6c7b19
lenkamo <102872288+lenkamo@users.noreply.github.com>
2022-04-23 12:22:03 +0200

    Create extensions.py
    Postprocessing - histogram

---
+++ extensions.py
@@ -0,0 +1,11 @@
+def get_histogram(clm, rule_id):
+  target = clm.kwargs["target"]
+  df = clm.kwargs["df"]
+  keys = sorted(df[target].unique())
+  values = clm.result["rules"][rule_id-1]["params"]["hist"]
+  hist = pd.Series(values, keys)
+  return hist
+  
+def plot_histogram(clm, rule_id):
+  hist = get_histogram(clm, rule_id)
+  hist.plot(kind = "bar")
